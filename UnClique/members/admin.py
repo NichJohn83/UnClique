fb00169cd147aa00dd0573c78a8cf22f70e9fc8e
Nicholas Johnson <nich.john83@gmail.com>
2018-06-22 05:28:16 -0400

    added landing page and changed backend db to mysql

---
+++ UnClique/members/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from members.models import member
+
+admin.site.register(member)
---
+++ UnClique/members/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+
+class member(models.Model):
+
+    FIRST = 'first'
+    SECOND = 'second'
+    THIRD = 'third'
+    FOURTH = 'fourth'
+    FIFTH_OR_HIGHER = 'fifth+'
+    GRAD = 'grad'
+    FACULTY = 'faculty'
+    STAFF = 'staff'
+    CLASSIFICATION_CHOICES = (
+        (FIRST, 'First Year'),
+        (SECOND, 'Second Year'),
+        (THIRD, 'Third Year'),
+        (FOURTH, 'Fourth Year'),
+        (FIFTH_OR_HIGHER, 'Fifth Year or higher'),
+        (GRAD, 'Graduate Student'),
+        (FACULTY, 'Faculty'),
+        (STAFF, 'Staff'),
+    )
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    preferred_name = models.CharField(max_length=30)
+    major = models.CharField(max_length=30)
+    classification = models.CharField(
+        max_length=10,
+        choices=CLASSIFICATION_CHOICES,
+        default=FIRST)
